d1a52a4f7f9294b9d3f40a6151d762c207763214
DipCrai <95180035+DipCrai@users.noreply.github.com>
2026-02-23 21:43:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 FLATPAK=true
+TWEAKS=true
 for arg in "$@"; do 
     case $arg in 
         --no-flatpak) 
             FLATPAK=false 
             ;;
+        --no-tweaks) 
+            TWEAKS=false 
+            ;;
         --help)
             echo "Usage: $0 [options]"
             echo
             echo "Options:"
-            echo "  --no-flatpak    Disable installation of Flatpak and GNOME Software."
+            echo "  --no-flatpak    Disable installation of Flatpak and Gnome Software."
+            echo "  --no-tweaks     Disable installation of Gnome Tweaks app."
             echo "  --help          Show this help message and exit."
             echo
             echo "Requirements:"
@@ -21,9 +26,13 @@ for arg in "$@"; do
 done
 
 sudo pacman -S --noconfirm gnome-shell gdm networkmanager \
-    gnome-console gnome-control-center gnome-tweaks \
+    gnome-console gnome-control-center \
     gnome-backgrounds nano
     
+if [ "$TWEAKS" = true ]; then
+    sudo pacman -S --noconfirm gnome-tweaks
+fi
+
 if [ "$FLATPAK" = true ]; then
     sudo pacman -S --noconfirm flatpak gnome-software
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
